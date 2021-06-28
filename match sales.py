0d0df9b2d3bb90d1230a3bb07a7a86eb311125bf
sam2611 <84084388+sam2611@users.noreply.github.com>
2021-06-28 17:31:42 +0530

    Add files via upload

---
+++ match sales.py
@@ -0,0 +1,16 @@
+
+import collections
+n = int(input().strip())
+
+ar = list(map(int, input().rstrip().split()))
+frequency = collections.Counter(ar)
+list1 = list(frequency.values())
+result=0
+r=0
+for x in list1:
+    if x%2==0:
+        r=x/2
+    else:
+        r=(x-1)/2
+    result=result+r
+print(int(result))
